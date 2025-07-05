ef39f866a50a6b32d3017eaa4ee174ff92b01d23
sobadfish <3098143605@qq.com>
2025-07-05 10:31:04 +0800

    解决缺少的文件

---
+++ src/main/java/org/sobadfish/teleportgun/form/CustomButtonForm.java
@@ -0,0 +1,80 @@
+package org.sobadfish.teleportgun.form;
+
+import cn.nukkit.Player;
+import cn.nukkit.form.element.ElementButton;
+import cn.nukkit.form.response.FormResponse;
+import cn.nukkit.form.response.FormResponseSimple;
+import cn.nukkit.form.window.FormWindow;
+import cn.nukkit.form.window.FormWindowSimple;
+import cn.nukkit.item.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class CustomButtonForm implements ICustomForm<FormResponseSimple> {
+
+    public int formId;
+
+    public String title;
+
+
+    public String content;
+
+    public Item item;
+
+    public List<ElementButton> elements = new ArrayList<>();
+
+    public Player playerInfo;
+
+    public CustomButtonForm(String title, String content, Player playerInfo,Item item) {
+        this.title = title;
+        this.item  = item;
+        this.content = content;
+        this.playerInfo = playerInfo;
+
+
+    }
+
+
+
+    public void addButton(ElementButton element) {
+        elements.add(element);
+    }
+
+    public abstract void callback(FormResponseSimple response);
+
+    public void callbackData(FormResponse response) {
+        if (response instanceof FormResponseSimple) {
+            callback((FormResponseSimple) response);
+        }
+    }
+
+
+    @Override
+    public Player getPlayerInfo() {
+        return playerInfo;
+    }
+
+    @Override
+    public void setFormId(int formId) {
+        this.formId = formId;
+    }
+
+    @Override
+    public int getFormId() {
+        return formId;
+    }
+
+    @Override
+    public FormWindow asWindows() {
+        FormWindowSimple simple = new FormWindowSimple(title, content);
+        for (ElementButton element : elements) {
+            simple.addButton(element);
+        }
+        return simple;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/org/sobadfish/teleportgun/form/ICustomForm.java
@@ -0,0 +1,29 @@
+package org.sobadfish.teleportgun.form;
+
+import cn.nukkit.Player;
+import cn.nukkit.form.response.FormResponse;
+import cn.nukkit.form.window.FormWindow;
+import cn.nukkit.item.Item;
+
+public interface ICustomForm<T extends FormResponse> {
+
+    void callbackData(FormResponse response);
+
+    int getFormId();
+
+    /**
+     * 创建表单
+     * */
+    void onCreateView();
+
+    void setFormId(int formId);
+
+    FormWindow asWindows();
+
+
+    Player getPlayerInfo();
+
+
+    boolean isCanRemove();
+
+}
